bb568f957e6f3be9cc8e5618893869bd2b05a24b
rsleitao <renato_sleitao@hotmail.com>
2024-02-15 10:17:30 +0000

    15_02_v2

--- animacoes.js
+++ animacoes.js
@@ -2,7 +2,7 @@ var owl = $('.owl-carousel');
 owl.owlCarousel({
     items:6,
     loop:true,
-    margin:12,
+    margin:40,
     autoplay:true,
     autoplayTimeout:4000,
     autoplayHoverPause:true
