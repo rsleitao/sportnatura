d48c0d07c560dab58ee5781ba9deb57ed34f91b2
rsleitao <renato_sleitao@hotmail.com>
2024-02-19 15:48:10 +0000

    19_02_v5

--- animacoes.js
+++ animacoes.js
@@ -7,3 +7,14 @@ owl.owlCarousel({
     autoplayTimeout:3000,
     autoplayHoverPause:true
 });
+
+wow = new WOW(
+    {
+    boxClass:     'wow',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  )
+  wow.init();
